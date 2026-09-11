3beba6748ac27ef856576ea4ed7d333c080c0ba4
Aoran Hu <48747984+Hocnonsense@users.noreply.github.com>
2026-09-11 15:45:00 +0000

    perf: check numpy only if required

--- src/yte/value_handler.py
+++ src/yte/value_handler.py
@@ -1,43 +1,55 @@
 from typing import Any
+from sys import modules
 
 
-try:
-    import numpy as np
-
-    def _handle_numpy_str(value):
-        if isinstance(value, np.str_):
-            return str(value)
+class HandleValue:
+    @staticmethod
+    def scalar(value):
         return value
 
-    def _handle_numpy_array(value):
-        if isinstance(value, np.ndarray):
-            return value.tolist()
-        return value
 
-except ImportError:
-
-    def _handle_numpy_str(value):
-        return value
-
-    def _handle_numpy_array(value):
-        return value
+_detected = False
+_handle_value = HandleValue()
 
 
 class ValueHandler:
+
+    @classmethod
+    def detect_numpy_import(cls):
+        global _detected
+
+        if _detected:
+            return
+        if "numpy" in modules:
+            import numpy as np  # type: ignore
+
+            class HandleNumpyValue(HandleValue):
+                @staticmethod
+                def scalar(value):
+                    if isinstance(value, np.ndarray):
+                        return value.tolist()
+                    if isinstance(value, np.generic):
+                        return value.item()
+                    return value
+
+            global _handle_value
+            _handle_value = HandleNumpyValue()
+            _detected = True
+
     def postprocess(self, value: Any) -> Any:
+        self.detect_numpy_import()
+        return self._postprocess(value)
+
+    def _postprocess(self, value: Any) -> Any:
         if isinstance(value, list):
-            return list(map(self.postprocess, value))
+            return list(map(self._postprocess, value))
         elif isinstance(value, dict):
             return {
-                self.postprocess(key): self.postprocess(value)
+                self._postprocess(key): self._postprocess(value)
                 for key, value in value.items()
             }
         else:
             return self.postprocess_atomic_value(value)
 
     def postprocess_atomic_value(self, value: Any) -> Any:
-        value = _handle_numpy_array(value)
-        if isinstance(value, list):
-            return list(map(_handle_numpy_str, value))
-        else:
-            return _handle_numpy_str(value)
+        return self._postprocess(_handle_value.scalar(value))
