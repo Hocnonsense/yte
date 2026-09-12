a87c07f464e5909484b7c30354c3f415211a0a13
Aoran Hu <48747984+Hocnonsense@users.noreply.github.com>
2026-09-12 11:28:07 +0000

    fix: simplify

--- src/yte/value_handler.py
+++ src/yte/value_handler.py
@@ -2,42 +2,37 @@ from typing import Any
 from sys import modules
 
 
-class HandleValue:
-    @staticmethod
-    def scalar(value):
-        return value
+def scalar(value):
+    return value
 
 
-_detected = False
-_handle_value = HandleValue()
+def detect_numpy_import():
+    if "numpy" in modules:
+        import numpy as np  # type: ignore
+
+        def numpy_scalar(value):
+            if isinstance(value, np.ndarray):
+                return value.tolist()
+            if isinstance(value, np.generic):
+                return value.item()
+            return value
+
+        def blank():
+            pass
+
+        global _scalar, _detect_numpy_import
+        _scalar = numpy_scalar
+        _detect_numpy_import = blank
+
+
+_scalar = scalar
+_detect_numpy_import = detect_numpy_import
 
 
 class ValueHandler:
 
-    @classmethod
-    def detect_numpy_import(cls):
-        global _detected
-
-        if _detected:
-            return
-        if "numpy" in modules:
-            import numpy as np  # type: ignore
-
-            class HandleNumpyValue(HandleValue):
-                @staticmethod
-                def scalar(value):
-                    if isinstance(value, np.ndarray):
-                        return value.tolist()
-                    if isinstance(value, np.generic):
-                        return value.item()
-                    return value
-
-            global _handle_value
-            _handle_value = HandleNumpyValue()
-            _detected = True
-
     def postprocess(self, value: Any) -> Any:
-        self.detect_numpy_import()
+        _detect_numpy_import()
         return self._postprocess(value)
 
     def _postprocess(self, value: Any) -> Any:
@@ -52,4 +47,7 @@ class ValueHandler:
             return self.postprocess_atomic_value(value)
 
     def postprocess_atomic_value(self, value: Any) -> Any:
-        return self._postprocess(_handle_value.scalar(value))
+        value = _scalar(value)
+        if isinstance(value, (list, dict)):
+            return self._postprocess(value)
+        return value
